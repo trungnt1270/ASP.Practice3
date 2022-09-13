05aca8df5a866daf2f579cf864778bed25fe3ef1
trungnt1270 <71129889+trungnt1270@users.noreply.github.com>
2022-09-13 21:08:03 +0700

    Practice3
    Practice3

---
+++ ProductManage/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductManage.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int CategoryID { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; }
+
+        //Navigation prop
+        public virtual ICollection<Product> Products { get; set; }
+
+    }
+}
